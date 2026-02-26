b58771d5f2d4fd6316b449d30f6787ea72058e8b
Nabanita Saha <nsaha1604@gmail.com>
2026-02-26 15:10:53 +0530

    dashboard changed

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,9 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/api-monitor/'
+
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/api-monitor/'
-
+  // Ensures built assets use the correct sub-path
+  base: '/dashboard',
+  server: {
+    // This is optional for production, but harmless to keep
+    allowedHosts: ['developer.agristack.gov.in'],
+  },
+  build: {
+    // Optional but recommended for cleaner builds
+    outDir: 'dist',
+    sourcemap: false,
+  },
 })
